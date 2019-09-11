a06e238b0c2f7e05ceff4ee61edb2307f310c23c
josias <kologojosias@gmail.com>
2019-09-11 14:06:48 +0100

    Handling payment done

--- freshniso/urls.py
+++ freshniso/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
-from .views import ItemDetailView, CheckoutView, HomeView, add_to_cart, remove_from_cart, OrderSummaryView, remove_single_item_from_cart
+from .views import (
+    ItemDetailView, CheckoutView, HomeView,
+    add_to_cart, remove_from_cart, 
+    OrderSummaryView, 
+    remove_single_item_from_cart, PaymentView
+)
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -10,5 +15,6 @@ urlpatterns = [
     path('product/<slug>/', ItemDetailView.as_view(), name="product"),
     path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart'),
     path('remove-from-cart/<slug>/', remove_from_cart, name='remove-from-cart'),
-    path('remove-single-item-from-cart/<slug>/', remove_single_item_from_cart, name='remove-single-item-from-cart')
+    path('remove-single-item-from-cart/<slug>/', remove_single_item_from_cart, name='remove-single-item-from-cart'),
+    path('payment/<payment_option>/', PaymentView.as_view(), name='payment')
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- freshniso/views.py
+++ freshniso/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Item, OrderItem, Order, BillingAddress
+from .models import Item, OrderItem, Order, BillingAddress, Payment
 from django.views.generic import ListView, DetailView, View
 from django.utils import timezone
 from django.contrib import messages
@@ -7,9 +7,10 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .forms import CheckoutForm
+from django.conf import settings
 
-
-# Create your views here.
+import stripe
+stripe.api_key = settings.STRIPE_TEST_KEY
 
 
 def products(request):
@@ -30,7 +31,7 @@ class CheckoutView(View):
     def post(self, *args, **kwargs):
         form = CheckoutForm(self.request.POST or None)
         try:
-            order = Order.Objects.get(self.request.user, ordered=False)
+            order = Order.objects.get(user=self.request.user, ordered=False)
             if form.is_valid():
                 street_address = form.cleaned_data.get('street_address')
                 apartment_address = form.cleaned_data.get('apartment_address')
@@ -51,12 +52,80 @@ class CheckoutView(View):
                 order.billing_address = billing_address
                 order.save()
                 # Add a redirect to the selected payment option.
-                return redirect("checkout")
-            messages.warning(self.request, "Failed checkout")
-            return redirect("checkout")
+                if payment_option == 'S':
+                    return redirect('payment', payment_option='stripe')
+                elif payment_option == 'P':
+                    return redirect('payment', payment_option='paypal')
+                else:
+                    messages.warning(self.request, "Invalid payment option selected")
+                    return redirect("checkout")
         except ObjectDoesNotExist:
             messages.error(self.request, "You don't have an active order")
             return redirect('order-summary')
+
+
+
+class PaymentView(View):
+    def get(self, *args, **kwargs):
+        order = Order.objects.get(user=self.request.user, ordered=False)
+        return render(self.request, "freshniso/payment.html", {
+            'order': order
+        })
+
+    def post(self, *args, **kwargs):
+        order = Order.objects.get(user=self.request.user, ordered=False)
+        token = self.request.POST.get('stripeToken')
+        amount = int(order.get_total() + 100)
+        try:
+            charge = stripe.Charge.create(
+            amount=amount,
+            currency="eur",
+            source=token,
+            ) 
+            order.ordered = True
+
+            # create payment
+            payment = Payment()
+            payment.stripe_charge_id = charge['id']
+            payment.user = self.request.user
+            payment.amount = order.get_total()
+            payment.save()
+
+            # assign the payment to the order
+
+            order.ordered = True
+            order.payment = payment
+            order.save()
+            messages.success(self.request, "Your order was successfull !")
+
+ 
+        except stripe.error.CardError as e:
+            # Since it's a decline, stripe.error.CardError will be caught
+            body = e.json_body
+            err  = body.get('error', {})
+            messages.error(self.request, f"{err.get('message')}")
+        except stripe.error.RateLimitError as e:
+            # Too many requests made to the API too quickly
+            messages.error(self.request, "Rate Limit error")
+        except stripe.error.InvalidRequestError as e:
+            # Invalid parameters were supplied to Stripe's API
+            messages.error(self.request, "Invalid parameters")
+        except stripe.error.AuthenticationError as e:
+            # Authentication with Stripe's API failed
+            # (maybe you changed API keys recently)
+            messages.error(self.request, "Authentication Error")
+        except stripe.error.APIConnectionError as e:
+            # Network communication with Stripe failed
+            messages.error(self.request, "Api Connection Error")
+        except stripe.error.StripeError as e:
+            # Display a very generic error to the user, and maybe send
+            # yourself an email
+            messages.error(self.request, "Something went wrong. You were not charge. Please Try again")
+        except Exception as e:
+            # Send email to, ourselves
+            messages.error(self.request, "A serious error occured. You have been noticed")
+
+        return redirect("/")
 
 
 class HomeView(ListView):
